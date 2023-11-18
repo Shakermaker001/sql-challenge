5b96718c9895ccf405e50be41099763b0a2dcfc5
Shakermaker001 <142272543+Shakermaker001@users.noreply.github.com>
2023-11-18 17:01:16 -0600

    That is final, I swear.

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -64,9 +64,6 @@ where d.dept_name = 'Sales'
 --List the frequency counts, in descending order, of all the employee last names 
 --(that is, how many employees share each last name)
 
---List the frequency counts, in descending order, of all the employee last names 
---(that is, how many employees share each last name)
-
 select last_name, count(last_name) as last_name_count
 from employees
 where last_name is NOT NULL
